214f7877e1cd30611bd9721150c2a63aa2c1d239
Soumadip <39thdecember@gmail.com>
2025-07-23 19:23:00 +0530

    Add a SQL DB ✔️

---
+++ db/db.go
@@ -0,0 +1,24 @@
+// Adding a SQL DB
+
+package db
+
+import (
+	"database/sql"
+	_ "github.com/mattn/go-sqlite3"
+)
+
+var DB *sql.DB
+ 
+func InitDB() {
+    var err error
+    DB, err = sql.Open("sqlite3", "api.db")
+ 
+    if err != nil {
+        panic("Could not connect to database🔴.")
+    }
+ 
+    DB.SetMaxOpenConns(10)
+    DB.SetMaxIdleConns(5)
+ 
+    //createTables()
+}
